e325b37f67b656f0fd855c780f65c8b6dfb6d3f3
PhantomOz <faniogor@gmail.com>
2024-07-17 01:16:20 +0100

    This adds the new params to pricefeed script

--- README.md
+++ README.md
@@ -23,3 +23,7 @@ forge script script/PriceFeed.s.sol --rpc-url $RPC_URL --private-key $PRIVATE_KE
 
 - _Peer Token `0x17975FB494576ae89D627F904Ec723B87c7C35c8`_
 - _Price Feed `0x1C9f974DF781C6EB3764F21Fe961ba38305213df`_
+
+New Addresses
+- _Peer Token `0x1EC66e52D13c18809F023948f1ae053025D2c969`_
+- _Price Feed `0x4417E9B86Be5d09331eF8B5a98Af4589228F476E`_
--- script/PriceFeed.s.sol
+++ script/PriceFeed.s.sol
@@ -8,6 +8,37 @@ import {PeerToken} from "../src/PeerToken.sol";
 contract PriceFeedScript is Script {
     bytes32[] priceFeeds;
     address[] tokens;
+    bytes32 public DAIUSD =
+        bytes32(
+            0x87a67534df591d2dd5ec577ab3c75668a8e3d35e92e27bf29d9e2e52df8de412
+        );
+    bytes32 public LINKUSD =
+        bytes32(
+            0x83be4ed61dd8a3518d198098ce37240c494710a7b9d85e35d9fceac21df08994
+        );
+    bytes32 public WBTCUSD =
+        bytes32(
+            0xea0459ab2954676022baaceadb472c1acc97888062864aa23e9771bae3ff36ed
+        );
+    bytes32 public USDCUSD =
+        bytes32(
+            0x41f3625971ca2ed2263e78573fe5ce23e13d2558ed3f2e47ab0f84fb9e7ae722
+        );
+    address public DAI = 0x57a8f8b6eD04e92f053C19EFbF1ab8C0314Fe7b0;
+    address public LINK = 0x1Fb9EEe6DF9cf79968D2b558AeDE454384498e2a;
+    address public WBTC = 0x45d341D33624Cc53B1E61f73C076f8A545DA191D;
+    address public USDC = 0x1Fb9EEe6DF9cf79968D2b558AeDE454384498e2a;
+
+    function setUp() public {
+        priceFeeds.push(DAIUSD);
+        priceFeeds.push(LINKUSD);
+        priceFeeds.push(WBTCUSD);
+        priceFeeds.push(USDCUSD);
+        tokens.push(DAI);
+        tokens.push(LINK);
+        tokens.push(WBTC);
+        tokens.push(USDC);
+    }
 
     function run() public {
         vm.startBroadcast();
